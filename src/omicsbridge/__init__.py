056d83c09ae9d19018922be2957beef5a56bc183
Witold Wolski <wewolski@gmail.com>
2025-11-13 22:54:33 +0100

    created validator

--- src/omicsbridge/__init__.py
+++ src/omicsbridge/__init__.py
@@ -1,3 +1,11 @@
 """Omics Bridge - Convert between AnnData and SummarizedExperiment formats."""
 
 __version__ = "0.1.0"
+
+from omicsbridge.column_resolver import ColumnResolver
+from omicsbridge.validator import validate_anndata_omics
+
+__all__ = [
+    "ColumnResolver",
+    "validate_anndata_omics",
+]
